30011bc88f8653f4c3a35df2df1833e2732f6ceb
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-09-05 11:00:12 +0900

    add: contailnerディレクトリに3つのページに対応するReactファイルを作成

---
+++ frontend/src/containers/Foods.jsx
@@ -0,0 +1,9 @@
+import { Flagment } from 'react';
+
+export const Foods = () => {
+  return (
+    <Flagment>
+      フード一覧
+    </Flagment>
+  )
+}
---
+++ frontend/src/containers/Orders.jsx
@@ -0,0 +1,9 @@
+import { Flagment } from 'react';
+
+export const Orders = () => {
+  return (
+    <Flagment>
+      注文画面
+    </Flagment>
+  )
+}
