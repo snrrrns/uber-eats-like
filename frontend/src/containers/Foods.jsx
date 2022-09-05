49f20c3ad32eab040aafb3aaa60b3bf761ece9f7
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-09-05 11:58:02 +0900

    update: matchからrestaurantIdを受け取って表示するように変更

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,9 +1,14 @@
 import React, { Fragment } from 'react';
 
-export const Foods = () => {
+export const Foods = ({
+  match
+}) => {
   return (
     <Fragment>
       フード一覧
+      <p>
+        resutaurantIdは{ match.params.restaurantsId }です
+      </p>
     </Fragment>
   )
 }
